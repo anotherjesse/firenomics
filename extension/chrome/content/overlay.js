8bf6e9f6456208de4291d459b910b93a86db893e
Manish Singh <yosh@gimp.org>
2008-12-14 15:12:36 -0800

    Move reporter functionality into a JS module

--- extension/chrome/content/overlay.js
+++ extension/chrome/content/overlay.js
@@ -1,4 +1,4 @@
-FIRENOMICS_URL = "http://firenomics.appspot.com"
+Components.utils.import("resource://firenomics/reporter.js");
 
 function fnLoad() {
   var appcontent = window.document.getElementById("appcontent");
@@ -11,21 +11,26 @@ function fnLoad() {
 }
 
 function fnInit(event) {
-	var win = new XPCNativeWrapper(event.originalTarget, "top");
-	
-	if (win.location.href.match(FIRENOMICS_URL + "/profile")) {
-		fnRenderProfilePage(win);
-		return;
-	}
+  var win = new XPCNativeWrapper(event.originalTarget, "top");
+
+  if (win.location.href.match(FIRENOMICS_URL + "/profile")) {
+    fnRenderProfilePage(win);
+    return;
+  }
 }
 
 function fnGotoProfilePage() {
   alert('going to profile page');
-  openUILinkIn(FIRENOMICS_URL + "/profile/foo", "tab");
+  openUILinkIn(FirenomicsReporter.FIRENOMICS_URL + "/profile/foo", "tab");
 }
 
 function fnRenderProfilePage(win) {
   alert('rendering profile page');
+}
+
+function fnSubmit() {
+  alert('submitting stuffs');
+  FirenomicsReporter.submit();
 }
 
 fnLoad();
