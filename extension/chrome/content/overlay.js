aac933dabe024e959a1f5782c329234915adcc17
Ian Fischer <itfische@gmail.com>
2008-12-14 18:14:20 -0800

    Made the extension insert the signed value into the page

--- extension/chrome/content/overlay.js
+++ extension/chrome/content/overlay.js
@@ -4,6 +4,8 @@ var Firenomics = (function() {
   var self = this;
 
   var load = function() {
+    // TODO: get rid of this!!!
+    //FirenomicsReporter.auth.clear();
     var appcontent = window.document.getElementById("appcontent");
     if (appcontent) {
       if (!appcontent.firenomicsInited) {
@@ -18,24 +20,78 @@ var Firenomics = (function() {
 
     if (win.location.href.match(FIRENOMICS_URL + "/profile")) {
       renderProfilePage(win);
-      return;
     }
   }
 
   self.gotoProfilePage = function() {
-    openUILinkIn(FIRENOMICS_URL + "/profile/agpmaXJlbm9taWNzcg0LEgdQcm9maWxlGA0M", "tab");
+    user = FirenomicsReporter.auth.get();
+    openUILinkIn(FIRENOMICS_URL + "/profile/" + user.key, "tab");
   }
 
   var renderProfilePage = function(doc) {
-    input = doc.getElementById('fnProfileSecret');
-    input.value = '1234';
-    div = doc.getElementById('fnClaimProfile');
-    div.style.visibility = 'visible';
+    var user = FirenomicsReporter.auth.get();
+    if (doc.location.href.match(user.key)) {
+      input = doc.getElementById('fnProfileSecret');
+      input.value = sign(input.value, user.secret);
+      div = doc.getElementById('fnClaimProfile');
+      div.style.visibility = 'visible';
+    }
+  }
+  
+  var sign = function(str, key) {
+    return FlockCryptoHash.md5(str + key);
   }
 
   self.submit = function() {
     FirenomicsReporter.submit();
   }
+
+  const CC = Components.classes;
+  const CI = Components.interfaces;
+  const CR = Components.results;
+
+  var EXPORTED_SYMBOLS = ["FlockCryptoHash"];
+
+  var FlockCryptoHash = {
+    hashStream: function FlockCryptoHash_hashStream(aStream, aAlgorithm) {
+      var hasher = CC["@mozilla.org/security/hash;1"]
+        .createInstance(CI.nsICryptoHash);
+      hasher.init(aAlgorithm);
+
+      hasher.updateFromStream(aStream, aStream.available());
+      var hash = hasher.finish(false);
+
+      var ret = "";
+      for (var i = 0; i < hash.length; ++i) {
+        var hexChar = hash.charCodeAt(i).toString(16);
+        if (hexChar.length == 1) {
+          ret += "0";
+        }
+        ret += hexChar;
+      }
+
+      return ret;
+    },
+    hash: function FlockCryptoHash_hash(aString, aAlgorithm) {
+      var stream = CC["@mozilla.org/io/string-input-stream;1"]
+        .createInstance(CI.nsIStringInputStream);
+      stream.setData(aString, aString.length);
+      return this.hashStream(stream, aAlgorithm);
+    },
+    md5Stream: function FlockCryptoHash_md5Stream(aStream) {
+      return this.hashStream(aStream, CI.nsICryptoHash.MD5);
+    },
+    md5: function FlockCryptoHash_md5(aString) {
+      return this.hash(aString, CI.nsICryptoHash.MD5);
+    },
+
+    hexSHA1FromStream: function FlockCryptoHash_hexSHA1FromStream(aStream) {
+      return this.hashStream(aStream, CI.nsICryptoHash.SHA1);
+    },
+    sha1: function FlockCryptoHash_sha1(aString) {
+      return this.hash(aString, CI.nsICryptoHash.SHA1);
+    }
+  };
 
   load();
 
